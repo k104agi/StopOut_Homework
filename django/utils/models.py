f115e99eb1b00278fdc75eb308ef41b479c248f3
kjs <k104agi@gmail.com>
2018-02-18 15:07:09 +0900

    config, util 폴더 잡고 토대 설정

---
+++ django/utils/models.py
@@ -0,0 +1,26 @@
+class EpisodeData:
+    """
+    하나의 에피소드에 대한 정보를 갖도록 함
+    """
+
+    episode_id =
+    url_thumbnail =
+    title =
+    rating =
+    created_date =
+
+    def __init__(self, episode_id, url_thumbnail, title, rating, created_date):
+        ...
+
+
+    webtoon_id =
+    page =
+
+def get_episode_list(webtoon_id, page):
+    """
+    고유ID(URL에서 titleId값)에 해당하는 웹툰의
+    특정 page에 있는 에피소드 목록을 리스트로 리턴
+    """
+    result = []
+    for i in range(에피소드마다?):
+        result.append(뭐시기)
